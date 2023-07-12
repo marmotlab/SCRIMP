f10dba2013412352330c743d222729d481343130
Wang Yutong <56498200+wyt2019suzhou@users.noreply.github.com>
2023-07-12 12:20:07 +0700

    Update runner.py

--- runner.py
+++ runner.py
@@ -67,7 +67,7 @@ class Runner(object):
                 mb_message.append(self.message)
                 mb_mask.append(self.comm_mask)
 
-                actions, ps, values_in, values_ex, values_all, pre_block, output_state, num_invalid, self.message = \
+                actions, ps, values_in, values_ex, values_all, pre_block, self.hidden_state, num_invalid, self.message = \
                     self.local_model.step(self.obs, self.vector, self.valid_actions, self.hidden_state,
                                           self.episodic_buffer.no_reward, self.message, self.num_agent, self.comm_mask)
                 self.one_episode_perf['invalid'] += num_invalid
@@ -81,7 +81,7 @@ class Runner(object):
                 rewards, self.valid_actions, self.obs, self.vector, self.train_valid, self.done, blockings, \
                     num_on_goals, self.one_episode_perf, max_on_goals, action_status, modify_actions, on_goal \
                     = one_step(self.env, self.one_episode_perf, actions, pre_block, self.local_model, values_all,
-                               output_state, ps, self.episodic_buffer.no_reward, self.message, self.episodic_buffer,
+                               self.hidden_state, ps, self.episodic_buffer.no_reward, self.message, self.episodic_buffer,
                                self.num_agent, self.comm_mask)
 
                 new_xy = self.env.get_positions()
